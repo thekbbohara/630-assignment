33a45ad71f2cb5f105d4f78b6f3e7d9cb3257d72
kbzcraft <ytkbzcraft@gmail.com>
2024-08-04 07:05:41 +0545

    Q2 -> solved : count total

--- javascript/array/level4.js
+++ javascript/array/level4.js
@@ -9,3 +9,14 @@ const output = arr.filter((item)=>{
   return item != "shyam" 
 })
 console.log(output)
+
+//Q2 count total strings which have length 4
+//expected output : 1
+let total = 0
+arr.forEach((item)=>{
+    // // yedi item.length === 4, total ma add 1
+    if(item.length === 4) {
+    total++
+  }
+})
+console.log({total})
