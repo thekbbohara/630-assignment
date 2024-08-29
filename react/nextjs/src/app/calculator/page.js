18a2b72e88c3900eae6e86b394ef950251737e15
kbzcraft <ytkbzcraft@gmail.com>
2024-08-29 06:23:28 +0545

    calc page'

--- react/nextjs/src/app/calculator/page.js
+++ react/nextjs/src/app/calculator/page.js
@@ -3,7 +3,6 @@ import CalculatorClient from "@/components/calculatorClient";
 const Calculator = () => {
   return (
     <main className="min-h-svh flex items-center">
-
       <CalculatorClient />
     </main>
   );
